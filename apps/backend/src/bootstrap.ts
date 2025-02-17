c2d7fc8c2543245333ce4e904697ab8c128d519c
Yannick <huyiyan1986@gmail.com>
2025-02-17 13:01:03 +0100

    added typescript

--- apps/backend/src/bootstrap.ts
+++ apps/backend/src/bootstrap.ts
@@ -1,6 +1,6 @@
 import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module.js';
 
 export const bootstrap = async () => {
   const app: INestApplication = await NestFactory.create(AppModule);
